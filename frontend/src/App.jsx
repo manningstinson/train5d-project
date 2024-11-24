d30374084adbe19de850ea2aecad93a59c22ddd6
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-11-24 07:08:54 +0000

    added directory structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container mx-auto px-4'>
+      <h1 className='text-3xl font-bold my-4'>Welcome to Your App</h1>
+    </div>
+  )
+}
+
+export default App
